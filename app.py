ec36530c7e5d901a751f70df7ee666073447cefe
Arko Saha <64651886+arko-saha@users.noreply.github.com>
2020-08-18 22:48:06 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import urllib
+from urllib.request import urlopen
+
+def connectivity():
+    
+    try:
+        urlopen('https://www.google.com', timeout=1)
+        return True
+    
+    except urllib.error.URLError as Error:
+        print(Error)
+        return False
+    
+    
+if connectivity():
+    print("URL is up!")
+else:
+    print("Couldn't reach it. Can you please check?")
